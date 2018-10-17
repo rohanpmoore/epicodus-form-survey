c21b71877dde9389d74aa7ea44b0205293e9f272
Robert Lee and Rohan Moore <robert@>
2018-10-17 13:51:44 -0700

    add javascript logic

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,20 @@
 $(document).ready(function(){
+  $("#surveyForm").submit(function(event) {
+    var name = $("input#fullName").val();
+    var born = $("#born").val();
+    var drink = $("#beverage").val();
+    var color = $("#color").val();
+    var gender = $("input:radio[name=gender]:checked").val();
 
+    $(".nameField").text(name);
+    $(".birthField").text(born);
+    $(".drinkField").text(drink);
+    $(".colorField").text(color);
+    $(".genderField").text(gender);
 
+    $("#infoReturn").show();
+    $("#surveyField").hide();
 
-  
+    event.preventDefault();
+  });
 });
